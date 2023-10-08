7bf91b082bf9e7e9ac942317563302336dde2833
Nate Sobeck <nsobeck910@gmail.com>
2023-10-08 10:16:52 -0400

    add initial StarshipList component and created route

---
+++ src/components/StarshipList.jsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react"
+import { getStarships } from "../../services/sw-api"
+
+const StarshipList = () => {
+  return (  
+    <h1>Starship List</h1>
+  )
+}
+
+export default StarshipList
